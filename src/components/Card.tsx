a9bf2af0fa5556285efdf11d11e1c75398214bb2
Alejo <alejoivanrojas@gmail.com>
2023-01-23 12:02:47 -0300

    completed-clase17

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@ import { ICard } from "../types"
 const Card = ({product}: ICard) => {
   return (
     <div className='card'>
+        <button className="btn-add" onClick={()=>alert("Product added")} >+</button>
         <div className="img-card">
           <img src={product.image} alt="product-img" />
         </div>
